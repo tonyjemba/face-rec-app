ebb429e32e106fe3f24e10a0ee7e2bc499fc5dca
Tony Jemba <48022053+tonyjemba@users.noreply.github.com>
2022-01-11 08:48:55 +0300

    Update Navigation.js
    using ternary operator

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,20 +1,19 @@
 import React from 'react';
 
 const Navigation = ({ onRouteChange, isSignedIn }) => {
-	if (isSignedIn) {
-		return (
-			<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+	
+	isSignedIn?(
+	<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
 				<p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>sign out</p>
-			</nav>
-		);
-	} else {
-		return (
-			<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+	</nav>
+	):(
+	<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
 				<p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>sign in</p>
 				<p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>create account</p>
-			</nav>
-		);
-	}
+	</nav>
+	)
+	
+
 }
 
 export default Navigation;
